61454698cb689328e878c40fab03095ec2361b39
Beatriz Silva <beatrizsilva@AC-LP0022.local>
2018-06-29 17:13:04 -0300

    Include components

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,27 @@
 
 let template = `<template>
-  <div id="app">
+  <div id="app" class="corpo">
     <img src="./assets/logo.png">
-    <h1>{{ msg }}</h1>
+    <h1 class="centralizado">{{ msg }}</h1>
     <h2>Essential test</h2>
-    <ul>
-      <li v-for="foto in fotos">
-        <img :src="foto.url" :alt="foto.titulo">
-      </li>
+    <ul class="lista-fotos">
+      <li class="lista-fotos-item" v-for="foto in fotos">
+      <meu-painel :titulo="foto.titulo">
+        <img class="imagem-responsiva" :src="foto.url" :alt="foto.titulo">
+      </meu-painel>
+    </li>
     </ul>
   </div>
 </template> `;
 
 <script>
+import Painel from './components/shared/painel/Painel.vue';
+
 export default {
   name:'app',
+  components: {
+    'meu-painel': Painel
+  },
   data () {
     return {
       msg: 'Vue.js App',
@@ -56,5 +63,48 @@ li {
 
 a {
   color: #42b983;
+}
+
+.centralizado {
+    text-align: center;
+  }
+
+.corpo {
+  font-family: Helvetica, sans-serif;
+  margin: 0 auto;
+  width: 96%;
+}
+
+.imagem-responsiva {
+  width: 100%;
+}
+
+.lista-fotos {
+  list-style: none;
+}
+
+.lista-fotos .lista-fotos-item {
+  display: inline-block;
+}
+
+.painel {
+  padding: 0 auto;
+  border: solid 2px grey;
+  display: inline-block;
+  margin: 5px;
+  box-shadow: 5px 5px 10px grey;
+  width: 200px;
+  height: 100%;
+  vertical-align: top;
+  text-align: center;
+}
+
+.painel .painel-titulo {
+  text-align: center;
+  border: solid 2px;
+  background: lightblue;
+  margin: 0 0 15px 0;
+  padding: 10px;
+  text-transform: uppercase;
 }
 </style>
