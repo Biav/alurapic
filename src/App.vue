c18ff7ea5978daa09fdc89d3606aaf9c33d1ebd0
Beatriz Silva <beatrizsilva@AC-LP0022.local>
2018-06-29 14:04:28 -0300

    Simple Project of photos - vue

---
+++ src/App.vue
@@ -0,0 +1,57 @@
+
+let template = `<template>
+  <div id="app">
+    <img src="./assets/logo.png">
+    <h1>{{ msg }}</h1>
+    <h2>Essential test</h2>
+    <ul>
+      <li v-for="foto in fotos">
+        <img :src="foto.url" :alt="foto.titulo">
+      </li>
+    </ul>
+  </div>
+</template> `;
+
+<script>
+export default {
+  name:'app',
+  data () {
+    return {
+      msg: 'Vue.js App',
+      fotos: []
+    }
+  },
+  created(){
+    alert("teste") // Initializate with page
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+
+h1, h2 {
+  font-weight: normal;
+}
+
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+
+a {
+  color: #42b983;
+}
+</style>
